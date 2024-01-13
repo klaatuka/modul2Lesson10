23567445e413e2137df78a5ea0c3337e41b80333
Me <klaatuka82@gmail.com>
2024-01-13 09:52:25 +0300

    Initial Commit

---
+++ modul2Lesson10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  modul2Lesson10
+//
+//  Created by Давид Узунян on 13.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
