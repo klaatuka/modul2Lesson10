a5046f62ca2d3ad84023e3cad818e155aa185e31
Me <klaatuka82@gmail.com>
2024-01-19 13:23:17 +0300

    fin

---
+++ modul2Lesson10/LastShot.swift
@@ -0,0 +1,29 @@
+//
+//  LastShot.swift
+//  modul2Lesson10
+//
+//  Created by Давид Узунян on 19.01.2024.
+//
+
+import UIKit
+
+class LastShot: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
